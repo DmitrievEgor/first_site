db10369915eea0651be7a93581fabf810f8e0576
DmitrievEgor <120376302+DmitrievEgor@users.noreply.github.com>
2023-02-14 11:20:46 +0300

    Update translater.py
    changed code

--- translater.py
+++ translater.py
@@ -1,12 +1,14 @@
-a = str(input())
 lst_r = ["алгебра", "число", "процент", "отрицательный", "положительный"]
 lst_a = ["algebra", "number ", "percent", "negative", "positive"]
 lst_g = ["die Algebra", "die Zahl", "das Prozent", "negativ", "positiv"]
-for item in lst_r:
-    item = a
-    if item in lst_r:
+while True:
+    a = input()
+    if a in lst_r:
         print("англ - " + str(lst_a[lst_r.index(a)]))
         print("нем - " + str(lst_g[lst_r.index(a)]))
+    elif a == '0':
         break
+    else:
+        print("Такого слова нет в списке")
 
 
